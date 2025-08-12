bfbcc77617dd5f9de8c04c8b1b9a70b4427bc971
e <49845680+e24z@users.noreply.github.com>
2025-08-12 14:11:10 +0100

    feat: improve fetch command to output data frame

--- src/quant_fpl/cli.py
+++ src/quant_fpl/cli.py
@@ -1,10 +1,15 @@
 import typer
+import asyncio
+from quant_fpl.data.fpl_client import FPLClient
 
 app = typer.Typer()
 
 @app.command()
 def fetch():
-    print("Fetching bootstrap...")
+    """Fetch the raw element data frame. Output timestamped .json and .parquet. """
+    client = FPLClient()
+    df = asyncio.run(client.bootstrap())
+    print(df)
 
 def main():
     app()
