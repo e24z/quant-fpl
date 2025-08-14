89c0bf3205c3cc903bb9fc7a5ba743176146c5b2
e <49845680+e24z@users.noreply.github.com>
2025-08-14 15:20:42 +0100

    feat: add check command to cli

--- src/quant_fpl/cli.py
+++ src/quant_fpl/cli.py
@@ -2,6 +2,7 @@ import typer
 import asyncio
 from quant_fpl.data.fpl_client import FPLClient
 from quant_fpl.data.tidy import tidy_payload
+from quant_fpl.data.greedy_picker import assert_squad_constraints
 
 app = typer.Typer()
 
@@ -17,9 +18,16 @@ def tidy():
     """Return a tidied view of the element DataFrame"""
     client = FPLClient()
     payload = asyncio.run(client.bootstrap_payload())
-    from quant_fpl.data.tidy import tidy_payload
     df = tidy_payload(payload)
     print(df.head(10).to_string(index=False))
+
+@app.command()
+def check():
+    """assert that the tidied view conforms to squad constraints"""
+    client = FPLClient()
+    payload = asyncio.run(client.bootstrap_payload())
+    df = tidy_payload(payload)
+    assert_squad_constraints(df.head(15))
 
 def main():
     app()
